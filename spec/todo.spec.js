18ef45fc2d311f66868e04c20b130b49f49c71d7
Theo Pack <tf.pack@gmail.com>
2015-05-15 15:28:26 +0200

    Working test

--- spec/todo.spec.js
+++ spec/todo.spec.js
@@ -1,8 +1,9 @@
 'use strict';
 
 describe('Todos', function() {
+  var Todo = require('../lib/todo');
   it('A new todo list is empty', function() {
     var todo = new Todo('My List');
-    expect(todo.size).toBe(0);
+    expect(todo.size()).toBe(0);
   });
-}
+});
