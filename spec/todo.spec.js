7d308379301d39d172c577517a32b62be8f94271
Theo Pack <tf.pack@gmail.com>
2015-05-15 17:32:32 +0200

    Added tests

--- spec/todo.spec.js
+++ spec/todo.spec.js
@@ -2,8 +2,27 @@
 
 describe('Todos', function() {
   var Todo = require('../lib/todo');
+
   it('A new todo list is empty', function() {
     var todo = new Todo('My List');
     expect(todo.size()).toBe(0);
+  });
+
+  it('A todo list has a name', function() {
+    var todo = new Todo('My List');
+    expect(todo.name).toBe('My List');
+  });
+
+  it('Items can be added to a todo list', function() {
+    var todo = new Todo('My List');
+    todo.add('Something todo');
+    expect(todo.contains('Something todo')).toBe(true);
+  });
+
+  it('Items can be removed from a todo list', function() {
+    var todo = new Todo('My List');
+    todo.add('Something todo');
+    todo.remove('Something todo');
+    expect(todo.contains('Something todo')).toBe(false);
   });
 });
